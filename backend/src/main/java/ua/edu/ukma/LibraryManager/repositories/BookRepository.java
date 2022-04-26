a251a44163ed3332e9a5b909e256901797a9857f
Victor <slavco2.p@gmail.com>
2022-04-26 23:58:30 +0300

    Add queries to BookRepository

--- backend/src/main/java/ua/edu/ukma/LibraryManager/repositories/BookRepository.java
+++ backend/src/main/java/ua/edu/ukma/LibraryManager/repositories/BookRepository.java
@@ -1,7 +1,21 @@
 package ua.edu.ukma.LibraryManager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.edu.ukma.LibraryManager.models.Book;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, String> {
+
+    @Query(value  = "SELECT * FROM book WHERE isbn IN " +
+            "(SELECT book_isbn FROM book_subject_area WHERE subject_area_cipher = :cipher)",
+            nativeQuery = true)
+    List<Book> findBooksBySubjectArea(@Param("cipher") String subjectAreaCipher);
+
+    @Query(value  = "SELECT * FROM book WHERE isbn IN " +
+            "(SELECT book_isbn FROM book_author WHERE asuthor = :author)",
+            nativeQuery = true)
+    List<Book> findBooksByAuthor(@Param("author") String author);
 }
