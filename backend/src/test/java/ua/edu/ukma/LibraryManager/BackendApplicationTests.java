3385f20d024d61cec46e7ddf5e45592326c6f6d7
Victor <slavco2.p@gmail.com>
2022-04-26 17:01:41 +0300

    Backend part of application setup

---
+++ backend/src/test/java/ua/edu/ukma/LibraryManager/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.LibraryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
