80d2dfa00f8d8d1b2cc8a81d764611084385db9d
Victor <slavco2.p@gmail.com>
2022-04-29 15:29:33 +0300

    Implement finding reader's checkouts

--- backend/src/main/java/ua/edu/ukma/LibraryManager/repositories/CheckoutRepository.java
+++ backend/src/main/java/ua/edu/ukma/LibraryManager/repositories/CheckoutRepository.java
@@ -2,6 +2,7 @@ package ua.edu.ukma.LibraryManager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.edu.ukma.LibraryManager.models.domain.Checkout;
 
 import java.util.List;
@@ -9,7 +10,21 @@ import java.util.List;
 public interface CheckoutRepository extends JpaRepository<Checkout, Integer> {
 
 
-    @Query(value = "",
+    @Query(value = "SELECT checkout_history.checkout_number, checkout_history.checkout_start_date," +
+            " checkout_history.checkout_expected_finish_date, checkout_history.checkout_real_finish_date," +
+            " checkout_history.compensation, book.isbn, book.title, book_exemplar.inventory_number" +
+            " FROM checkout_history INNER JOIN book_exemplar" +
+            "  ON checkout_history.exemplar_inventory_number = book_exemplar.inventory_number" +
+            "  INNER JOIN book" +
+            "  ON book.isbn = book_exemplar.book_isbn" +
+            " WHERE reader_ticket_number IN" +
+            "          (SELECT ticket_number " +
+            "           FROM reader " +
+            "           WHERE principal_id IN" +
+            "               (SELECT principal_id" +
+            "                FROM principal" +
+            "                WHERE email = :searched_reader_email)" +
+            "          );",
             nativeQuery = true)
-    List<Object[]> findCheckoutsOfReader(String readerEmail);
+    List<Object[]> findCheckoutsOfReader(@Param("searched_reader_email") String readerEmail);
 }
--- backend/src/main/java/ua/edu/ukma/LibraryManager/services/CheckoutServiceImpl.java
+++ backend/src/main/java/ua/edu/ukma/LibraryManager/services/CheckoutServiceImpl.java
@@ -8,7 +8,13 @@ import org.springframework.transaction.annotation.Transactional;
 import ua.edu.ukma.LibraryManager.models.dto.checkout.CheckoutDetailsDTO;
 import ua.edu.ukma.LibraryManager.repositories.CheckoutRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -21,6 +27,22 @@ public class CheckoutServiceImpl implements CheckoutService {
     @Override
     public List<CheckoutDetailsDTO> getCheckoutOfUser(String readerEmail) {
         List<Object[]> checkoutsObj = checkoutRepository.findCheckoutsOfReader(readerEmail);
-        return null;
+        return checkoutsObj.stream().map(checkoutObj -> {
+            CheckoutDetailsDTO resCheckout = new CheckoutDetailsDTO();
+            resCheckout.setCheckoutNumber((Integer) checkoutObj[0]);
+            resCheckout.setCheckoutStartDate(LocalDate.parse(checkoutObj[1].toString()));
+            resCheckout.setCheckoutExpectedFinishDate(LocalDate.parse(checkoutObj[2].toString()));
+            Object realFinishDateObj = checkoutObj[3];
+            LocalDate realFinishDate =
+                    realFinishDateObj == null
+                    ? null
+                    : LocalDate.parse(realFinishDateObj.toString());
+            resCheckout.setCheckoutRealFinishDate(realFinishDate);
+            resCheckout.setCompensation((BigDecimal) checkoutObj[4]);
+            resCheckout.setBookIsbn((String) checkoutObj[5]);
+            resCheckout.setBookTitle((String) checkoutObj[6]);
+            resCheckout.setExemplarInventoryNumber((Integer) checkoutObj[7]);
+            return resCheckout;
+        }).collect(Collectors.toList());
     }
 }
