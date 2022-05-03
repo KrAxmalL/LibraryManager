7bd3b0b344aab83310028708b7aaa19794dc7a3b
Victor <slavco2.p@gmail.com>
2022-05-03 15:28:16 +0300

    Implement basic jwt manager

---
+++ backend/src/main/java/ua/edu/ukma/LibraryManager/security/jwt/JWTManager.java
@@ -0,0 +1,42 @@
+package ua.edu.ukma.LibraryManager.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+import ua.edu.ukma.LibraryManager.models.security.Principal;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JWTManager {
+
+    private static final long ACCESS_TOKEN_EXPIRATION_TIME_MILLIS = 10 * 60 * 1000;
+    private static final String CLAIM = "roles";
+
+    private final Algorithm algorithm;
+    private final JWTVerifier verifier;
+
+    public JWTManager() {
+        //todo: move secret to properties
+        this.algorithm = Algorithm.HMAC256("secret".getBytes(StandardCharsets.UTF_8));
+        this.verifier = JWT.require(algorithm).build();
+    }
+
+    public String getAccessToken(Principal principal, String issuer) {
+        return JWT.create()
+                  .withSubject(principal.getUsername())
+                  .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME_MILLIS))
+                  .withIssuer(issuer)
+                  .withClaim(CLAIM, principal.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                  .sign(algorithm);
+    }
+
+    public DecodedJWT verifyToken(String token) {
+        return verifier.verify(token);
+    }
+}
