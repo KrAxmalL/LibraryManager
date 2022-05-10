1652014a1e6a85d1147feb03eb41bf1f0be963f3
Victor <slavco2.p@gmail.com>
2022-05-10 17:54:26 +0300

    Move login logic from form to page

--- frontend/src/components/Login/LoginForm.js
+++ frontend/src/components/Login/LoginForm.js
@@ -1,73 +1,29 @@
-import React, { useState } from "react";
-
-import { useRef } from 'react';
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-
-import { authActions } from "../../store/auth-slice";
-
-import jwtDecode from "jwt-decode";
-
-import { login } from "../../api/authentication";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
 
 import classes from './LoginForm.module.css';
-import { Link, Navigate, useNavigate } from "react-router-dom";
 
-function LoginForm() {
-  const dispatch = useDispatch();
-
+function LoginForm(props) {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-  const [loginError, setLoginError] = useState(false);
-
-  const navigate = useNavigate();
-
-  const navigateAfterLogin = (roles) => {
-    if(!roles) {
-      return '/login';
-    }
-    else if(roles.includes('READER')) {
-      return '/reader/books';
-    }
-    else if(roles.includes('LIBRARIAN')) {
-      return '/librarian/books';
-    }
-    else if(roles.includes('ADMINISTRATOR')) {
-      return '/administrator/books';
-    }
-    else {
-      return '/unauthorized';
-    }
-  }
+  const loginError = props.loginError;
 
   const submitFormHandler = async (e) => {
     e.preventDefault();
 
     const email = emailInputRef.current.value;
     const password = passwordInputRef.current.value;
-
-    try {
-        const { accessToken } = await login(email, password);
-        const decodedToken = jwtDecode(accessToken);
-        const roles = decodedToken.roles;
-        dispatch(authActions.setAccessToken({ accessToken }));
-        dispatch(authActions.setRoles({ roles }));
-
-        setLoginError(false);
-        navigate({
-          pathname: navigateAfterLogin(roles)
-        });
-    } catch(error) {
-        setLoginError(true);
-    }
+    props.onLogin(email, password);
   }
 
     return (
         <form onSubmit={submitFormHandler} className={classes['login-form']}>
           <input className={classes.input} type='email' placeholder="Електронна пошта" id='email' required  ref={emailInputRef}></input>
           <input className={classes.input} type='password' placeholder="Пароль" id='password' required ref={passwordInputRef}></input>
-          {loginError && 
-            <p className={classes.error}>Не вдалось увійти в систему! Перевірте електронну пошта та пароль і спробуйте ще раз</p>
+          {loginError &&
+            <p className={classes.error}>Не вдалось увійти в систему!<br />
+                                         Перевірте електронну пошта та пароль і спробуйте ще раз
+            </p>
           }
           <input className={`${classes.input} ${classes.submit}`} type='submit' value='Увійти'></input>
           <Link className={classes.link} to='../register'>Зареєструватися</Link>
--- frontend/src/pages/public/Login.js
+++ frontend/src/pages/public/Login.js
@@ -1,18 +1,63 @@
-import React from "react";
+import jwtDecode from "jwt-decode";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../api/authentication";
 import Layout from "../../components/layout/Layout";
 import LoginForm from "../../components/login/LoginForm";
+import { authActions } from "../../store/auth-slice";
 
 import classes from './Login.module.css'
 
 function Login() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [loginError, setLoginError] = useState(false);
+
+    const navigateAfterLogin = (roles) => {
+        if(!roles) {
+            return '/login';
+        }
+        else if(roles.includes('READER')) {
+            return '/reader/books';
+        }
+        else if(roles.includes('LIBRARIAN')) {
+            return '/librarian/books';
+        }
+        else if(roles.includes('ADMINISTRATOR')) {
+            return '/administrator/books';
+        }
+        else {
+            return '/unauthorized';
+        }
+    }
+
+    const loginHandler = async (email, password) => {
+        try {
+            const { accessToken } = await login(email, password);
+            const decodedToken = jwtDecode(accessToken);
+            const roles = decodedToken.roles;
+            dispatch(authActions.setAccessToken({ accessToken }));
+            dispatch(authActions.setRoles({ roles }));
+
+            setLoginError(false);
+            navigate({
+              pathname: navigateAfterLogin(roles)
+            });
+        } catch(error) {
+            setLoginError(true);
+        }
+    }
+
     return (
         <Layout>
             <h1 className={classes['form-title']}>Увійти</h1>
             <div className="container">
                 <div className={`container text-center ${classes['middle-container']}`}>
                     <div className="container">
-                        <section id="content">
-                            <LoginForm />
+                        <section>
+                            <LoginForm onLogin={loginHandler} loginError={loginError} />
                         </section>
                     </div>
                 </div>
